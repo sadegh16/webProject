2c7fbcf685843fc9638ac84ddd99e999acd076e4
sadegh khorasani <mohammadsakh@gmail.com>
2018-12-07 14:24:02 +0330

    place folders

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import MainPage from "./mainPage/components/mainRoot";
-
+import MainPage from "./components/mainPage/components/mainPage.jsx";
 class App extends Component {
   render() {
     return (
